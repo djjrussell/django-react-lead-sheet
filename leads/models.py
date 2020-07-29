326836465876e217c7c595e0d4fb475f1edd5afe
djjrussell <djjrussell@gmail.com>
2020-07-29 14:39:41 -0700

    migrations added for user
    pre changing App into a login page

--- leads/models.py
+++ leads/models.py
@@ -12,14 +12,29 @@ class Company(models.Model):
     #     managed = False
 
 
+class User(models.Model):
+    title = models.CharField(max_length=255, default=None, blank=True, null=True)
+    first_name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    middle_name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    last_name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    username = models.CharField(max_length=255, default=None, blank=True, null=True)
+    email = models.CharField(max_length=255, default=None, blank=True, null=True)
+    password = models.CharField(max_length=255, default=None, blank=True, null=True)
+    role = models.CharField(max_length=255, default=None, blank=True, null=True)
+    status = models.CharField(max_length=255, default=None, blank=True, null=True)
+
+
 class Lead(models.Model):
     name = models.CharField(max_length=100, default=None, blank=True, null=True)
     tax_id = models.CharField(max_length=100, default=None, blank=True, null=True)
     email = models.EmailField(default=None, blank=True, null=True)
     message = models.CharField(max_length=300, default=None, blank=True, null=True)
     company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     # class Meta:
     #     managed = False
+
+
 
--- leads/views.py
+++ leads/views.py
@@ -4,6 +4,7 @@ from .models import *
 from .serializers import *
 from rest_framework import generics
 
+
 class LeadListCreate(generics.ListCreateAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
